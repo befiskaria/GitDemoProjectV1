d4081b3a6fe0b7d56f50f24d7baaf01f980c1a5b
befis <befis@BefisIdeaCentreA540>
2021-09-15 14:34:49 -0400

    First Commit

---
+++ src/test/java/testPackage/TestClass.java
@@ -0,0 +1,14 @@
+package testPackage;
+
+public class TestClass {
+
+	public static void main(String[] args) {
+String input="Automation Tester";
+StringBuilder input1=new StringBuilder();
+input1.append(input);
+input1.reverse();
+System.out.println(input1);
+
+	}
+
+}
