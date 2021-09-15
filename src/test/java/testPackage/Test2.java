0abe8d3d145898b05adb386a9c324b38193a47ee
electron <electron@electron>
2021-09-15 16:01:08 -0400

    Added additional checks..

--- src/test/java/testPackage/Test2.java
+++ src/test/java/testPackage/Test2.java
@@ -3,7 +3,7 @@ package testPackage;
 public class Test2 {
 
 	public static void main(String[] args) {
-		String input="Automation Tester";
+		String input="Automation";
 		StringBuilder input1=new StringBuilder();
 		input1.append(input);
 		input1.reverse();
